975b86a68617adee4a91011ce77e145780f1d318
Demba-Diakhate <diakhatedemba55@gmail.com>
2025-12-02 12:44:55 +0000

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="icon" href="{{ asset('img/icon.png') }}" type="image/x-icon">
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
@@ -24,7 +25,7 @@
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
         <style>[x-cloak] { display: none !important; }</style>
-        
+
     </head>
     <body class="bg-gray-100">
 
